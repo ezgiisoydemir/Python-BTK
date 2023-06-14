c0e3c041344c5258cc87c893bb604d26a4806e60
Ezgi Soydemir <41003405+ezgiisoydemir@users.noreply.github.com>
2023-06-14 17:23:03 +0300

    Update strings.py

--- strings.py
+++ strings.py
@@ -8,9 +8,10 @@ print(greeting)
 length = len(greeting)
 print(greeting[0])
 print(greeting[3])
-print(greeting[length-1])
+print(greeting[length-1]) #Sonn karakteri aldik. 0'dan basladigi icin 1 eksiltmek gerek
 print(greeting[-1])
-print(greeting[3:7])
-print(greeting[3:])
-print(greeting[:15])
-print(greeting[2:40:2])
+print(greeting[3:7])  #3'ten baslar ve 7.index'e kadar gider. Sonuc => name
+print(greeting[3:])   #3'ten basla sonuna kadar git
+print(greeting[:15])    #Bastan basla 15.karaktere kadar git
+print(greeting[2:40:2])   #2'den 40'a kadar git. Ama 2 karakterde birini al
+print(greeting[2:40:3])   #3 karakterden birini alir
