e8704b27fc7222f629f7c9e82b098ed9abdc0661
Ezgi Soydemir <41003405+ezgiisoydemir@users.noreply.github.com>
2023-06-14 17:29:13 +0300

    Update format.py

--- format.py
+++ format.py
@@ -1,10 +1,23 @@
 name = "Ezgi"
 surname = "Soydemir"
 age =22
-# print("My name is {} {}" .format(name,surname))
-# print("My name is {1} {0}" .format(name,surname))
-#print("My name is {s} {n}" .format(n=name,s=surname))
-#print("My name is {} {} and I'm {} years old" .format(name,surname,age))
+
+print("My name is {} {}" .format(name,surname))
+# My name is Ezgi Soydemir
+
+print("My name is {0} {1}" .format(name,surname))
+# name index 0 , surname index 1
+# My name is Ezgi Soydemir
+
+print("My name is {1} {0}" .format(name,surname))
+# Index yerlerini degistirdik
+# My name is Soydemir Ezgi 
+
+print("My name is {s} {n}" .format(n=name,s=surname))
+# My name is Soydemir Ezgi 
+
+print("My name is {} {} and I'm {} years old" .format(name,surname,age))
+# My name is Ezgi Soydemir and i am 22 years old
 
 result = 200/700
 print("the result is {r:1.3}".format(r=result))
